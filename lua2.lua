a6da71eb5b93891caed9e2fce62fb970fd457118
huhhunter777-eng <huhhunter777@gmail.com>
2026-07-23 10:48:06 +0500

    Update lua2.lua

--- lua2.lua
+++ lua2.lua
@@ -1,1 +1,3 @@
-
+--W JNKIE LOL SKID 
+getgenv().SCRIPT_KEY = "665ee78b-6b41-4857-8ea4-a2f8a4115716";
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/352ac539eb547e0261506e66c88bdbd1e78cbdb5d5cba73e967645f493aea2a3/download"))()
